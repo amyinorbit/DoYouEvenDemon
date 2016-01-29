95878afbe9f9f88f16a1c1f0483d4b724c361d7f
Amy Parent <amy@amyparent.com>
2016-01-29 20:15:21 +0000

    Initial Commit

---
+++ DoYouEvenDemon/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DoYouEvenDemon
+//
+//  Created by Cesar Parent on 29/01/2016.
+//  Copyright © 2016 cesarparent. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
